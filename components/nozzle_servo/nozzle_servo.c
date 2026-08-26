56a14ae3dade4c9971a6aaed9418d0a65420675c
Harit Krishan <haritkrishan176@gmail.com>
2026-08-27 01:05:58 +1000

    added cleanup logic

--- components/nozzle_servo/nozzle_servo.c
+++ components/nozzle_servo/nozzle_servo.c
@@ -19,13 +19,14 @@ static mcpwm_oper_handle_t s_pOperator = NULL;
 static mcpwm_cmpr_handle_t s_pComparator = NULL;
 static mcpwm_gen_handle_t s_pGenerator = NULL;
 
+static bool s_isInitialized = false;
 
 static esp_err_t s_setPulseWidth(uint32_t ulPulseWidthUs) {
     esp_err_t lErr = ESP_OK;
     
 
     // check if nozzle servo initialized
-    if(NULL == s_pComparator) {
+    if(!s_isInitialized) {
         ESP_LOGE(TAG, "Nozzle servo not initialized. Cannot set pulse width!");
         lErr = ESP_ERR_INVALID_STATE;
 
@@ -39,8 +40,33 @@ static esp_err_t s_setPulseWidth(uint32_t ulPulseWidthUs) {
         goto end_set_pulse_width;
     }
 
+    ESP_LOGI(TAG, "Set pulse width to %u", ulPulseWidthUs);
+
 end_set_pulse_width:
     return lErr;
+}
+
+
+static void s_cleanup(void) {
+    if(NULL != s_pGenerator) {
+        mcpwm_del_generator(s_pGenerator);
+        s_pGenerator = NULL;
+    }
+
+    if(NULL != s_pComparator) {
+        mcpwm_del_comparator(s_pComparator);
+        s_pComparator = NULL;
+    }
+
+    if(NULL != s_pOperator) {
+        mcpwm_del_operator(s_pOperator);
+        s_pOperator = NULL;
+    }
+
+    if(NULL != s_pTimer) {
+        mcpwm_del_timer(s_pTimer);
+        s_pTimer = NULL;
+    }
 }
 
 
@@ -168,8 +194,16 @@ esp_err_t nozzleServoInit(void) {
         goto end_init;
     }
 
+    s_isInitialized = true;
+
+    ESP_LOGI(TAG, "Nozzle servo initialized on pin %d", PIN_NOZZLE_SERVO);
 
 end_init:
+
+    if(lErr) {
+        s_cleanup();
+    }
+
     return lErr;
 }
 
@@ -180,6 +214,8 @@ esp_err_t nozzleServoExtend(void) {
     lErr = s_setPulseWidth(NOZZLE_SERVO_EXTEND_US);
     if(lErr) {
         ESP_LOGE(TAG, "Failed to extend. Code: 0x%X", lErr);
+    } else {
+        ESP_LOGI(TAG, "Nozzle servo extending...");
     }
 
     return lErr;
@@ -192,6 +228,8 @@ esp_err_t nozzleServoRetract(void) {
     lErr = s_setPulseWidth(NOZZLE_SERVO_RETRACT_US);
     if(lErr) {
         ESP_LOGE(TAG, "Failed to retract. Code: 0x%X", lErr);
+    } else {
+        ESP_LOGI(TAG, "Nozzle servo retracting...");
     }
 
     return lErr;
@@ -204,6 +242,8 @@ esp_err_t nozzleServoStop(void) {
     lErr = s_setPulseWidth(NOZZLE_SERVO_STOP_US);
     if(lErr) {
         ESP_LOGE(TAG, "Failed to stop nozzle servo. Code: 0x%X", lErr);
+    } else {
+        ESP_LOGI(TAG, "Nozzle servo stopped.");
     }
 
     return lErr;
