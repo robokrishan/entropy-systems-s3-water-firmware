c04bd15173d7c7fd798bc56f0f79ad19e3fb2c4f
robokrishan <haritkrishan176@gmail.com>
2026-08-28 14:33:21 +1000

    refactored state position unknown

--- components/state_machine_states/state_machine_state_pos_unknown.c
+++ components/state_machine_states/state_machine_state_pos_unknown.c
@@ -1,22 +1,44 @@
 #include "state_machine_state_pos_unknown.h"
+#include "nozzle_servo.h"
 #include "esp_log.h"
+
 
 static const char *TAG = "SM_POS_UNKNOWN";
 
 
-StateMachineStateId_t statePositionUnknownProcessEvent(StateMachineEvent_t *pEvent) {
+/* state initialization */
+static esp_err_t s_stateInit(void) {
+    esp_err_t lErr = ESP_OK;
+
+    lErr = nozzleServoStop();
+    if(lErr) {
+        ESP_LOGE(TAG, "failed to init state. Code: 0x%X", lErr);
+    }
+
+    return lErr;
+}
+
+
+/* state deinitialization */
+static esp_err_t s_stateDeinit(void) {
+
+
+    return ESP_OK;
+}
+
+
+/* event processing */
+static void s_stateProcess(StateMachineEvent_t *pEvent) {
     switch (pEvent->eId) {
 
         case SM_EVENT_NOZZLE_EXTEND:
-            ESP_LOGI(TAG, "Extending nozzle from unknown position");
-
-            return STATE_MACHINE_LOWERING;
+            ESP_LOG_EVENT(*pEvent);
+            break;
 
 
         case SM_EVENT_NOZZLE_RETRACT:
-            ESP_LOGI(TAG, "Retracting nozzle from unknown position");
-
-            return STATE_MACHINE_RAISING;
+            ESP_LOG_EVENT(*pEvent);
+            break;
 
 
         case SM_EVENT_UPPER_LIMIT_ACTIVE:
@@ -24,9 +46,8 @@ StateMachineStateId_t statePositionUnknownProcessEvent(StateMachineEvent_t *pEve
              * Upper limit switch provides a known
              * physical reference position.
              */
-            ESP_LOGI(TAG, "Upper limit active. Nozzle position resolved as STOWED");
-
-            return STATE_MACHINE_STOWED;
+            ESP_LOG_EVENT(*pEvent);
+            break;
 
 
         case SM_EVENT_LOWER_LIMIT_ACTIVE:
@@ -34,9 +55,8 @@ StateMachineStateId_t statePositionUnknownProcessEvent(StateMachineEvent_t *pEve
              * Lower limit switch provides a known
              * physical reference position.
              */
-            ESP_LOGI(TAG, "Lower limit active. Nozzle position resolved as DEPLOYED");
-
-            return STATE_MACHINE_DEPLOYED;
+            ESP_LOG_EVENT(*pEvent);
+            break;
 
 
         default:
@@ -44,6 +64,37 @@ StateMachineStateId_t statePositionUnknownProcessEvent(StateMachineEvent_t *pEve
                 stateMachineEventName(*pEvent)
             );
 
+            break;
+    }
+}
+
+
+/* next state */
+static StateMachineStateId_t s_stateNextState(const StateMachineEvent_t *pEvent) {
+    switch(pEvent->eId) {
+        case SM_EVENT_NOZZLE_EXTEND:
+            return STATE_MACHINE_LOWERING;
+
+        case SM_EVENT_NOZZLE_RETRACT:
+            return STATE_MACHINE_RAISING;
+
+        case SM_EVENT_UPPER_LIMIT_ACTIVE:
+            return STATE_MACHINE_STOWED;
+
+        case SM_EVENT_LOWER_LIMIT_ACTIVE:
+            return STATE_MACHINE_DEPLOYED;
+
+        default:
             return STATE_MACHINE_POSITION_UNKNOWN;
     }
 }
+
+
+/* state definition */
+StateMachineState_t g_stateMachineStatePositionUnknown = {
+    .eState = STATE_MACHINE_POSITION_UNKNOWN,
+    .cbInit = s_stateInit,
+    .cbDeinit = s_stateDeinit,
+    .cbProcess = s_stateProcess,
+    .cbNextState = s_stateNextState
+};
