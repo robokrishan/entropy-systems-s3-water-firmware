61c047126d731a9626fc74e9fb6fa249ed1bb06e
robokrishan <haritkrishan176@gmail.com>
2026-08-28 15:15:04 +1000

    state registrations implemented, refactor complete.

---
+++ components/state_machine_states/include/state_machine_states.h
@@ -0,0 +1,9 @@
+#ifndef COMPONENTS_STATE_MACHINE_STATES_H_
+#define COMPONENTS_STATE_MACHINE_STATES_H_
+
+#include "esp_err.h"
+
+
+esp_err_t stateMachineStatesRegister(void);
+
+#endif /* COMPONENTS_STATE_MACHINE_STATES_H_ */
---
+++ components/state_machine_states/state_machine_states.c
@@ -0,0 +1,83 @@
+#include "state_machine_states.h"
+#include "state_machine.h"
+
+#include "state_machine_state_init.h"
+#include "state_machine_state_stowed.h"
+#include "state_machine_state_lowering.h"
+#include "state_machine_state_deployed.h"
+#include "state_machine_state_pumping.h"
+#include "state_machine_state_raising.h"
+#include "state_machine_state_pos_unknown.h"
+#include "state_machine_state_fault.h"
+
+#include "esp_log.h"
+
+
+static const char* TAG = "SM_STATES";
+
+
+esp_err_t stateMachineStatesRegister(void) {
+    esp_err_t lErr = ESP_OK;
+
+    // register init state as initial state
+    lErr = stateMachineRegisterState(&g_stateMachineStateInit, true);
+    if(lErr) {
+        ESP_LOGE(TAG, "Failed to register INIT state");
+        goto end_register;
+    }
+
+    // register stowed state
+    lErr = stateMachineRegisterState(&g_stateMachineStateStowed, false);
+    if(lErr) {
+        ESP_LOGE(TAG, "Failed to register STOWED state");
+        goto end_register;
+    }
+
+    // register lowering state
+    lErr = stateMachineRegisterState(&g_stateMachineStateLowering, false);
+    if(lErr) {
+        ESP_LOGE(TAG, "Failed to register LOWERING state");
+        goto end_register;
+    }
+
+    // register deployed state
+    lErr = stateMachineRegisterState(&g_stateMachineStateDeployed, false);
+    if(lErr) {
+        ESP_LOGE(TAG, "Failed to register DEPLOYED state");
+        goto end_register;
+    }
+
+    // register pumping state
+    lErr = stateMachineRegisterState(&g_stateMachineStatePumping, false);
+    if(lErr) {
+        ESP_LOGE(TAG, "Failed to register PUMPING state");
+        goto end_register;
+    }
+
+    // register raising state
+    lErr = stateMachineRegisterState(&g_stateMachineStateRaising, false);
+    if(lErr) {
+        ESP_LOGE(TAG, "Failed to register RAISING state");
+        goto end_register;
+    }
+
+    // register position unknown state
+    lErr = stateMachineRegisterState(&g_stateMachineStatePositionUnknown, false);
+    if(lErr) {
+        ESP_LOGE(TAG, "Failed to register POS_UNKNOWN state");
+        goto end_register;
+    }
+
+    // register fault state
+    lErr = stateMachineRegisterState(&g_stateMachineStateFault, false);
+    if(lErr) {
+        ESP_LOGE(TAG, "Failed to register FAULT state");
+        goto end_register;
+    }
+
+    ESP_LOGI(TAG, "All states registerd in state machine");
+
+end_register:
+
+    return lErr;
+}
