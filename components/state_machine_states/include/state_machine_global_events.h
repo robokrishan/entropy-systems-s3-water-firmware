b37d892744306a046a63377d45342f1725d891ea
Harit Krishan <haritkrishan176@gmail.com>
2026-08-28 23:38:10 +1000

    implemented global fault event handling

---
+++ components/state_machine_states/include/state_machine_global_events.h
@@ -0,0 +1,13 @@
+#ifndef COMPONENTS_STATE_MACHINE_GLOBAL_EVENTS_H_
+#define COMPONENTS_STATE_MACHINE_GLOBAL_EVENTS_H_
+
+#include "stdbool.h"
+#include "state_machine_common.h"
+
+
+bool stateMachineGlobalEventProcess(StateMachineEvent_t* pEvent);
+
+bool stateMachineGlobalEventGetNextState(const StateMachineEvent_t* pEvent, StateMachineStateId_t* pNextState);
+
+
+#endif /* COMPONENTS_STATE_MACHINE_GLOBAL_EVENTS_H_ */
---
+++ components/state_machine_states/state_machine_global_events.c
@@ -0,0 +1,41 @@
+#include "state_machine_global_events.h"
+#include "esp_log.h"
+
+
+static const char* TAG = "SM_GLOBAL";
+
+
+bool stateMachineGlobalEventProcess(StateMachineEvent_t* pEvent) {
+    if(NULL == pEvent) {
+        ESP_LOGE(TAG, "Invalid event pointer");
+        return false;
+    }
+
+    switch(pEvent->eId) {
+        case SM_EVENT_FAULT:
+            ESP_LOGI(TAG, "Processing global event: %s", stateMachineEventName(*pEvent));
+            return true;
+
+        default:
+            return false;
+    }
+}
+
+bool stateMachineGlobalEventGetNextState(
+    const StateMachineEvent_t* pEvent, 
+    StateMachineStateId_t* pNextState
+) {
+    if((NULL == pEvent) || (NULL == pNextState)) {
+        ESP_LOGE(TAG, "Invalid argument");
+        return false;
+    }
+
+    switch(pEvent->eId) {
+        case SM_EVENT_FAULT:
+            *pNextState = STATE_MACHINE_FAULT;
+            return true;
+
+        default:
+            return false;
+    }
+}
