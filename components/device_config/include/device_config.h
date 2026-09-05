567e94d12d7e70ff6ae07638c35d720541cd19dd
robokrishan <haritkrishan176@gmail.com>
2026-09-05 17:04:09 +1000

    re-assigned pins for fresh breadboard

--- components/device_config/include/device_config.h
+++ components/device_config/include/device_config.h
@@ -1,10 +1,10 @@
 #include "driver/gpio.h"
 
-#define CONFIG_PIN_NOZZLE_SERVO         GPIO_NUM_1
-#define CONFIG_PIN_PUMP                 GPIO_NUM_13
-#define CONFIG_PIN_UPPER_LIMIT          GPIO_NUM_12
-#define CONFIG_PIN_LOWER_LIMIT          GPIO_NUM_11
+#define CONFIG_PIN_NOZZLE_SERVO         GPIO_NUM_7
+#define CONFIG_PIN_PUMP                 GPIO_NUM_6
+#define CONFIG_PIN_UPPER_LIMIT          GPIO_NUM_1
+#define CONFIG_PIN_LOWER_LIMIT          GPIO_NUM_2
 #define CONFIG_PIN_RC_INPUT_PUMP        GPIO_NUM_3
 #define CONFIG_PIN_RC_INPUT_NOZZLE      GPIO_NUM_2
-#define CONFIG_PIN_I2C_SDA              GPIO_NUM_8
-#define CONFIG_PIN_I2C_SCL              GPIO_NUM_9
+#define CONFIG_PIN_I2C_SDA              GPIO_NUM_13
+#define CONFIG_PIN_I2C_SCL              GPIO_NUM_12
